db193fcafb847e51f65b6f5433510c6ef2b2cab1
Ching0309 <chinwithching@gmail.com>
2021-10-21 00:26:19 +0800

    retrofit APP

---
+++ app/src/main/java/com/example/retrofit/Book.java
@@ -0,0 +1,34 @@
+package com.example.retrofit;
+
+public class Book {
+
+
+
+    private String msg;
+    private int code;
+    private String request;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+}
---
+++ app/src/main/java/com/example/retrofit/RetrofitService.java
@@ -0,0 +1,13 @@
+package com.example.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RetrofitService {
+        @GET("book/search")
+        Call<Book> getSearchBook(@Query("q") String name,
+                                 @Query("tag") String tag,
+                                 @Query("start") int start,
+                                 @Query("count") int count);
+}
